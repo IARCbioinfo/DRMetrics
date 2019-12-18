9b77403d45f8f84496f1da85c541921aadb83014
aurelieGabriel <gabrielaureli7@gmail.com>
2019-12-18 21:55:49 +0100

    replaced Seq_main and Seq_calcul functions by compute_SD and compute_SD_allSamples to update the method for SD values computation + SD_map_f updated to suit nextjournal

--- scripts/SEQ_DIFF.R
+++ scripts/SEQ_DIFF.R
@@ -1,276 +1,80 @@
-# __________
-#  Tools :
-# _________
-Merging_function <- function(l_data, dataRef){
-  colnames(dataRef)[1] <- "Sample_ID"
-  res_l_data <- list()
-  for (i in 1:length(l_data)){
-    c_data <- l_data[[i]]
-    colnames(c_data)[1] <- "Sample_ID"
-    if (dim(c_data)[1] != dim(dataRef)[1]){
-      warning(paste("Warning : the data frame[" ,  i ,")] doesn't have the same number of lines than `dataRef`. An inner join will be effecte") )
-    }
-    else if(sum(c_data[, 1] == dataRef[, 1]) != length(c_data[, 1])){
-      warning(paste("Sample_IDs in data frame [", i,"] and dataRef are not the same, or are not in the same order. An inner join will be effected.", sep =""))
-    }
-    data_m <- merge(dataRef, c_data, by = "Sample_ID")
-    dataRef <- dataRef[, 1:dim(dataRef)[2]]
-    r_data <- data_m[,(dim(dataRef)[2] + 1):dim(data_m)[2]]
-    r_data <- cbind(data_m[, 1], r_data)
-    colnames(r_data)[1] <- 'Sample_ID'
-    res_l_data[[i]] <- r_data
-  }
-  return(list(res_l_data, dataRef))
+
+compute_SD <- function(dist_space1,dist_space2,k){
+  #computes SD for a single given point
+  #dist_space1: distances in space 1
+  #dist_space2: distances in space 2
+  #k: number of neighbors considered
+  ranks_df = data.frame(Sample_ID=names(dist_space1), Space1=rank(dist_space1)-2, Space2=rank(dist_space2)-2, stringsAsFactors = F)
+  k_subset = ranks_df[order(ranks_df$Space1),][2:k,]
+  s1       = sum((k-k_subset$Space1)*abs(k_subset$Space1 - k_subset$Space2))
+  k_subset = ranks_df[order(ranks_df$Space2),][2:k,]
+  s2       = sum((k-k_subset$Space2)*abs(k_subset$Space1 - k_subset$Space2))
+  return( s1/2 + s2/2)
 }
 
-# ------------------------------
-
-############################################################################################
-############################################################################################
-Seq_calcul <- function( l_data, dataRef, listK){
-  
-  # __________ Clusters initialization ______
-  no_cores <- detectCores() # - 1
-  cl <- makeCluster(no_cores)
-  registerDoParallel(cl)
-  # _________________________________________
-  global_seq_list <- list()
-  for (I in 1:length(l_data)){
-    c_data <- l_data[[I]]
-    colnames(c_data)[1] <- 'Sample_ID'  ; colnames(dataRef)[1] <- 'Sample_ID'
-    if (dim(c_data)[1] != dim(dataRef)[1]){
-      warning("The number of lines between `c_data` and `dataRef` differs. A merge will be effected.")
-    }
-    else if (sum(c_data[, 1] == dataRef[, 1]) != length(c_data[, 1])){
-      warning("Sample_IDs in `c_data` and `dataRef` are not the same, or are not in the same order. An inner join will be effected.")
-    }
-    data_m <- merge(c_data, dataRef, by = 'Sample_ID')
-    ncol_c_data <- dim(c_data)[2]
-    c_data <- data_m[, 1:dim(c_data)[2]]
-    dataRef <- data_m[, (dim(c_data)[2]+1):dim(data_m)[2]]
-    dataRef <- cbind(data_m[, 1], dataRef)
-    colnames(dataRef)[1] <- 'Sample_ID'
-    #________________ Distances matrices __________
-    dist1 <- as.matrix(dist(c_data[, 2:dim(c_data)[2]], method = "euclidian", diag = TRUE, upper = TRUE))
-    rownames(dist1) <- as.character(c_data[ ,1])
-    colnames(dist1) <- as.character(c_data[ ,1])
+compute_SD_allSamples <- function(distRef,List_projection,k_values,colnames_res_df, threads=2){
+  #computes SD values for all samples and for multiple comparisons (each reduced space listed in List_projection compared to the reference space) 
+  #distRef: distances in the reference space
+  #List_projection: list of reduced spaces to compare to the reference space
+  #k_values: vector listing the k values corresponding to the number of neighbors considered
+  SD_comp=data.frame(Sample_ID=rep(rownames(distRef),each=length(k_values)),K=rep(k_values,nrow(distRef)))
+  for(df in 1:length(List_projection)){
+    space1 = List_projection[[df]]
+    dist1 = as.matrix(dist(space1, method = "euclidean", diag = TRUE, upper = TRUE))
     
-    dist2 <- as.matrix(dist(dataRef[, 2:dim(dataRef)[2]], method = "euclidian", diag = TRUE, upper = TRUE))
-    rownames(dist2) <- as.character(dataRef[ ,1])
-    colnames(dist2) <- as.character(dataRef[ ,1])
-    # ____________________________________________
-    seq_c_data <- data.frame()
-    seq_c_dataL <- foreach(i=1:length(listK)) %dopar% { #,.combine=rbind
-    #for(i in 1:length(listK)){
-    k <- listK[i]
-    colnames(c_data)[1] <- 'Sample_ID'  ; colnames(dataRef)[1] <- 'Sample_ID'
-    if (dim(c_data)[1] != dim(dataRef)[1]){
-      warning("The number of lines between `c_data` and `dataRef` differs. A merge will be effected")
+    # check that the two matrices of distances have the same ordering 
+    if(!all(rownames(dist1)==rownames(distRef))){
+      print("Error: The two matrices of distances do not have the same ordering!")
     }
-    else if (sum(c_data[, 1] == dataRef[, 1]) != length(c_data[, 1])){
-      warning("Sample_IDs in `c_data` and `dataRef` are not the same, or are not in the same order. An inner join will be effected.")
+    # For each sample compute SD metric for each k value
+    cl = makeCluster(threads)
+    registerDoParallel(cl)
+    samples_names = rownames(dist1)
+    SD_k_samples  = foreach(i=1:nrow(dist1), .combine=c, .export=c("compute_SD")) %dopar% {
+      SD_k_values = sapply(k_values,function(k) compute_SD(dist1[i, ],distRef[i, ],k) )
+      SD_k_values
     }
-    data_m <- merge(c_data, dataRef, by = 'Sample_ID')
-    ncol_c_data <- dim(c_data)[2]
-    c_data <- data_m[, 1:dim(c_data)[2]]
-    dataRef <- data_m[, (dim(c_data)[2]+1):dim(data_m)[2]]
-    dataRef <- cbind(data_m[, 1], dataRef)
-    colnames(dataRef)[1] <- 'Sample_ID'
-    #________________ Distances matrices __________
-    dist1 <- as.matrix(dist(c_data[, 2:dim(c_data)[2]], method = "euclidian", diag = TRUE, upper = TRUE))
-    rownames(dist1) <- as.character(c_data[ ,1])
-    colnames(dist1) <- as.character(c_data[ ,1])
-      
-    dist2 <- as.matrix(dist(dataRef[, 2:dim(dataRef)[2]], method = "euclidian", diag = TRUE, upper = TRUE))
-    rownames(dist2) <- as.character(dataRef[ ,1])
-    colnames(dist2) <- as.character(dataRef[ ,1])
-    # ____________________________________________
-
-    seq_diff_l <- c()
-    n <- dim(dist1)[1]
-    for (ii in 1:n){
-      c_point <- rownames(dist1)[ii]
-      N1_dist_l <- list(dist1[ii, ])[[1]]
-      N2_dist_l <- list(dist2[ii, ])[[1]]
-        
-      names(N1_dist_l) <- rownames(dist1)
-      names(N2_dist_l) <- rownames(dist2)
-      N1_dist_l <- sort(N1_dist_l)
-      N2_dist_l <- sort(N2_dist_l)
-        
-      N1_rank_l <- seq(length(N1_dist_l))
-      N2_rank_l <- seq(length(N2_dist_l))
-      names(N1_rank_l) <- names(N1_dist_l)
-      names(N2_rank_l) <- names(N2_dist_l)
-        
-      N1_rank_l <- N1_rank_l[1:k]
-      N2_rank_l <- N2_rank_l[1:k]
-        
-      N1_df <- data.frame("Sample_ID" = names(N1_rank_l) , "Rank1" = N1_rank_l)
-      N2_df <- data.frame("Sample_ID" = names(N2_rank_l) , "Rank2" = N2_rank_l)
-        
-      All_neighbors <- unique(c(as.character(N1_df$Sample_ID),as.character(N2_df$Sample_ID)))
-        
-      s1 = 0
-      s2 = 0
-      for (j in 1:length( All_neighbors)){
-        if (All_neighbors[j] %in%  N1_df$Sample_ID & All_neighbors[j] %in%  N2_df$Sample_ID ){
-          N1_index_j = which(N1_df$Sample_ID  == All_neighbors[j]  )
-          N2_index_j = which(N2_df$Sample_ID  == All_neighbors[j]  )
-  
-          if(  s1 + ((k - N1_df$Rank1[N1_index_j]) * abs(N1_df$Rank1[N1_index_j] - N2_df$Rank2[N2_index_j])) < s1 ){
-          }
-          s1 = s1 + ((k - N1_df$Rank1[N1_index_j]) * abs(N1_df$Rank1[N1_index_j] - N2_df$Rank2[N2_index_j]))
-          s2 = s2 + ((k - N2_df$Rank2[N2_index_j]) * abs(N1_df$Rank1[N1_index_j] - N2_df$Rank2[N2_index_j]))
-        }
-        else if (All_neighbors[j] %in%  N1_df$Sample_ID){ 
-          N1_index_j = which(N1_df$Sample_ID  == All_neighbors[j]  )
-          s1 = s1 + ((k - N1_df$Rank1[N1_index_j]) * abs(N1_df$Rank1[ N1_index_j]))
-          s2 = s2 
-        }
-        else{ 
-          N2_index_j = which(N2_df$Sample_ID  == All_neighbors[j]  )
-          s1 = s1 
-          s2 = s2 + ((k - N2_df$Rank2[N2_index_j]) * abs( N2_df$Rank2[N2_index_j]))
-        }
-        
-      }
-      S = 0.5 * s1 + 0.5 * s2
-
-      seq_diff_l <- c(seq_diff_l,  S)
-      }
-      seq_diff_k_df <- data.frame('Sample_ID' = c_data$Sample_ID, 'K' = rep(k, length(c_data$Sample_ID)), 'Seq' = seq_diff_l)
-
-     list(seq_diff_k_df)
-
-    }
-      seq_c_data = data.frame()
-    for (i in seq(1:length(seq_c_dataL))){
-        seq_c_data <- rbind(seq_c_data,seq_c_dataL[[i]][[1]] )
-    }
+    stopCluster(cl)
     
-    global_seq_list[[I]] <- seq_c_data
+    # merge 
+    SD_comp=cbind(SD_comp,SD_k_samples)
+    colnames(SD_comp)[ncol(SD_comp)]=colnames_res_df[df]
   }
-  stopCluster(cl)
-  return(global_seq_list)
+  return(SD_comp)
 }
-############################################################################################
 
-############################################################################################
-Seq_main <- function(l_data, dataRef, listK, colnames_res_df = NULL , filename = NULL , graphics = FALSE, stats = FALSE){
-  l_data <- Merging_function(l_data, dataRef)
-  L_data <- list()
-  for (i in 1:length(l_data[[1]])){
-    L_data[[i]] <- l_data[[1]][[i]]
-  }
-  dataRef <- l_data[[2]]
-  l_data <- L_data
-  
-  if (length(l_data) == 1 & stats == TRUE){
-    warning("Statistical option are not available if `l_data` length is equal to 1.")
-    stats =  FALSE
-  }
-  global_seq_list <- Seq_calcul(l_data , dataRef , listK )
-  for (i in 1:length(global_seq_list)){
-    global_seq_list[[i]] <- global_seq_list[[i]][complete.cases(global_seq_list[[i]]), ]
-  }
-  
-  # _______________ Writing _________________
-  df_to_write <- data.frame('Sample_ID' = global_seq_list[[1]]$Sample_ID, 'K' = global_seq_list[[1]]$K )
-  c_colnames <- c()
-  for (i in 1:length(global_seq_list)){
-    df_to_write <- cbind(df_to_write, global_seq_list[[i]]$Seq)
-    c_colnames <- c(c_colnames, paste('V', i, sep=""))
-
-  }
-  if (is.null(colnames_res_df) == FALSE){
-    colnames(df_to_write)[3:length(colnames(df_to_write))] <- colnames_res_df
-  }
-  if (is.null(colnames_res_df) == TRUE){
-    colnames(df_to_write)[3:dim(df_to_write)[2]] <- c_colnames
-  }
-  if (is.null(filename) == FALSE) {
-    if (file.exists(as.character(filename))){
-      warning("The filename gives as argument exist in the current directory, this name will be 'incremented'.")
-      c = 2
-      while(file.exists(as.character(filename))){
-        filename <- paste(filename, c, sep = "" )
-        c = c+1
-      }
-    }
-    write.table(df_to_write, file = filename, sep = "\t")
-  }
-  
-  data_Seq <- df_to_write
-  data_diff_mean_k <- data.frame("k" =  unique(data_Seq$K))
-  for (j in seq(from = 3, to = dim(data_Seq)[2], by = 1)) {
-    mean_by_k <- tapply(data_Seq[, j], data_Seq$K, mean)
-    data_diff_mean_k <- cbind(data_diff_mean_k, mean_by_k)
-  }
-  colnames(data_diff_mean_k)[2:length(colnames(data_diff_mean_k))] <- colnames(data_Seq)[3:dim(data_Seq)[2]]
-  if (graphics == FALSE & stats == FALSE){
-    return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k))
-  }
-  if (graphics == TRUE){
-    p <- Seq_graph_by_k('nothing', Names=colnames_res_df, data_diff_mean_K = data_diff_mean_k)
-
-  }
-  else{ # graphics == False
-    p <- 0 # Only to respect algorithm structure
-  }
-  if (graphics == TRUE & stats == FALSE){
-    return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k, 'graphics' = p))
-  }
-  if (stats == TRUE){
-    if(dim(data_diff_mean_k)[2] == 2){
-      warning("Statics cannot be computed if length list of `l_data` is smaller than two.")
-      if (graphics == TRUE){
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k, 'graphics' = p))
-      }
-      else{
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k))
-      }
-    }
-    if(dim(data_diff_mean_k)[2] == 3){
-      if(dim(data_diff_mean_k)[1] < 30){
-      WT = wilcox.test(data_diff_mean_k[,2], data_diff_mean_k[, 3], paired = TRUE)
-      print(WT)
-      }
-      else{
-        WT = t.test(data_diff_mean_k[,2], data_diff_mean_k[, 3], paired = TRUE)
-        print(WT)
-      }
-      if (graphics == TRUE){
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k, 'graphics' = p, 'paired_test' = WT))
-      }
-      else{
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k, 'paired_test' =  WT))
-      }
-    }
-    else{
-      pwt_df <- data.frame('mean_seq' = data_diff_mean_k[, 2], 'method'= rep(paste(colnames(data_diff_mean_k)[2], 2, sep = ""), dim(data_diff_mean_k)[1]))
-      for (i in 3:dim(data_diff_mean_k)[2]){
-        c_df <- data.frame('mean_seq' = data_diff_mean_k[, i], 'method'=rep(paste(colnames(data_diff_mean_k)[i], i, sep = ""), dim(data_diff_mean_k)[1]))
-        pwt_df <- rbind(pwt_df, c_df )
-      }
-      if (dim(data_diff_mean_k)[1] < 30){
-      paired_test_m  <- pairwise.wilcox.test(pwt_df$mean_seq, pwt_df$method,    paired = TRUE)$p.value #p.adj = "holm",
-      }
-      else{
-        paired_test_m  <-  pairwise.t.test(pwt_df$mean_seq, pwt_df$method,    paired = TRUE)$p.value #p.adj = "holm",
-        
-      }
-      if (graphics == TRUE){
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k, 'graphics' = p,  'pairwise_tests' = paired_test_m ))
-      }
-      else{
-        return(list('Seq_df' = df_to_write, 'Seq_mean_by_k' = data_diff_mean_k,  'pairwise_tests' = paired_test_m ))
-      }
-    }
-    warning('Unexpected request ')
-  }
+SD_map_f <- function(SD_df, Coords_df, legend_pos = "right" ){
+  # SD_df data frame such as :
+  #  col1 = Sample_ID, col2 = k, col3 = SD_values
+  # Coords_df data frame such as :
+  # col1 = Sample_ID, col2 = AxisX, col3 = AxisY
+  SD_tmp = SD_df
+  SD_tmp$Sample_ID = as.character( SD_tmp$Sample_ID)
+  SD_tmp           = SD_tmp[order(SD_tmp$Sample_ID),]
+  unique_sample_id = unique(SD_tmp$Sample_ID)
+  SDmeans_ID       = sapply(unique_sample_id, function(x){
+    mean(SD_tmp[which(SD_tmp$Sample_ID == x),3])
+  })
+  Coords_df <- Coords_df[names(SDmeans_ID),]
+  SD_Coords_df <- cbind(  Coords_df, "SD" = SDmeans_ID)
+  SD_Coords_df <- SD_Coords_df[order(SD_Coords_df$SD, decreasing = T),]
+  colnames(SD_Coords_df)[1:2] = c("V1","V2")
+  pSD_Main <- ggplot(  SD_Coords_df, aes(x=V1,y=V2,  color=SD/max(SD))) +  geom_point(size=4, alpha =.8) + scale_color_distiller(palette = "Spectral")
+  pSD_Main <- pSD_Main +  labs(title="", 
+                               x=colnames(Coords_df)[1], y=colnames(Coords_df)[2]) +
+    theme( legend.position = legend_pos,
+           plot.title=element_text(size=16, face="bold", hjust=0.5,lineheight=1.2),  # title
+           plot.subtitle =element_text(size=14, hjust=0.5),
+           plot.caption =element_text(size=12,  hjust=0.5),
+           axis.title.x=element_text(size=16),  # X axis title
+           axis.title.y=element_text(size=16),  # Y axis title
+           axis.text.x=element_text(size=14),  # X axis text
+           axis.text.y=element_text(size=14),
+           legend.text = element_text(size = 10) ,
+           legend.title = element_blank())
+  return(list(SD_Coords_df,pSD_Main))
 }
-############################################################################################
+
 
 ############################################################################################
 Seq_graph_by_k  <-function (data_Seq, Names=NULL, data_diff_mean_K = NULL, log = FALSE){
@@ -403,41 +207,7 @@ seq_permutation_test <- function(data, data_ref, list_K, n=30, graph = TRUE){
   return(list(WT,p))
 }
 
-############################################################################################################
 
-SD_map_f <- function(SD_df, Coords_df, legend_pos = "right" ){
-  
-  # SD_df data frame such as :
-  #  col1 = Sample_ID, col2 = k, col3 = SD_values
-  # Coords_df data frame such as :
-  # col1 = Sample_ID, col2 = AxisX, col3 = AxisY
-  
-  colnames(SD_df) <- c("Sample_ID", "k", "SD")
-  SD_df <- SD_df[order(SD_df$Sample_ID),]
-  SD_df$Sample_ID <- as.character( SD_df$Sample_ID)
-  unique_sample_id <- as.character(unique(SD_df$Sample_ID)) 
-  SDmeans_ID <- unlist(lapply(1:length(unique_sample_id), function(i){
-    mean(SD_df$SD[which(SD_df$Sample_ID == unique_sample_id [i])])
-  }))
-  colnames(Coords_df) <- c("Sample_ID", "V1", "V2")
-  Coords_df <- Coords_df[order(Coords_df$Sample_ID),]
-  SD_Coords_df <- cbind(  Coords_df, "SD" = SDmeans_ID)
-  SD_Coords_df <- SD_Coords_df[order(SD_Coords_df$SD, decreasing = T),]
-  pSD_Main <- ggplot(  SD_Coords_df, aes(x=V1, y=V2,  color=SD/max(SD))) +  geom_point(size=4, alpha =.8) + scale_color_distiller(palette = "Spectral")
-  pSD_Main <- pSD_Main +  labs(title="", 
-                               y=TeX("dim2"), x="dim1") +
-    theme( legend.position = legend_pos,
-           plot.title=element_text(size=16, face="bold", hjust=0.5,lineheight=1.2),  # title
-           plot.subtitle =element_text(size=14, hjust=0.5),
-           plot.caption =element_text(size=12,  hjust=0.5),
-           axis.title.x=element_text(size=16),  # X axis title
-           axis.title.y=element_text(size=16),  # Y axis title
-           axis.text.x=element_text(size=14),  # X axis text
-           axis.text.y=element_text(size=14),
-           legend.text = element_text(size = 10) ,
-           legend.title = element_blank())#+  guides(col = guide_legend( ncol = 2))  # Y axis text
-  
-  return(list(SD_Coords_df,pSD_Main))
-  
-  
-}
+
+
+
